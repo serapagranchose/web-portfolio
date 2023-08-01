4a9e3b4f11b866cf17a8b2e3d8bfd5def5ea8d4f
serapagranchose <seraphin.perrot@gmail.com>
2023-08-02 04:15:08 +0900

    :sparkles: Adding a bare skeleton of Sveltekit with Skeleton UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
-<script>
-    import Navbar from '$lib/Navbar.svelte'
-    import Readme from '$lib/Readme.svelte';
-</script>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<Navbar />
-
-<!-- <Readme /> -->
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Let's get cracking bones!</h1>
+		<p>Start by exploring:</p>
+		<ul>
+			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
+			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
+			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
+		</ul>
+	</div>
+</div>
